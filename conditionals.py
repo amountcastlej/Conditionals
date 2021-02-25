933c1426e5524774e580248183fc0409f4b237d2
Adam Joines <amountcastlej@gmail.com>
2021-02-24 21:01:43 -0500

    Python file added

---
+++ conditionals.py
@@ -0,0 +1,41 @@
+#x = 12
+#if x > 50:
+#    print("bigger than 50")
+#else: 
+#    print("smaller than 50")
+
+#x = 55
+#if x > 10:
+#    print("bigger than 10")
+#elif x > 50:
+#    print("bigger than 50")
+#else:
+#    print("smaller than 10")
+
+#if x < 10:
+#    print("smaller than 10")
+
+#x = float(input("Enter a number for x: "))
+#y = float(input("Enter a number for y: "))
+#if x == y:
+#    print("x and y are equal")
+#    if y != 0:
+#        print("therefore, x / y is", x/y)
+#elif x < y:
+#    print("x is smaller")
+#else: 
+#    print("y is smaller")
+#print("Thanks!")
+
+#x = 5
+#if x < 10:
+#    print("smaller")
+#if x > 20:
+#    print("bigger")
+#print("Finished")
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(("number is even"))
+else:
+    print("number is odd")
